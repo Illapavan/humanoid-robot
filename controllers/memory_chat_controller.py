015cd43740c751ab2cd21d3a209e041faff5029e
Illapavan <pavan@radiusagent.com>
2023-06-16 15:08:16 +0530

    added pinecone integration and added in memory chat to the bot

--- controllers/memory_chat_controller.py
+++ controllers/memory_chat_controller.py
@@ -1,29 +1,96 @@
 from flask import request, jsonify
 from utils.openai_util import ChatOpenAI
 from utils.session_util import SessionManager
+from utils.pinecone_utils import PineconeManager
+from langchain.agents import AgentType
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import SerpAPIWrapper
+from langchain.agents import initialize_agent
+from langchain.agents import Tool
+from langchain import OpenAI, LLMChain
+from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
+
 
 llm = ChatOpenAI(temperature=0.8)
 session_manager = SessionManager()
+pinecone = PineconeManager()
+
+search = SerpAPIWrapper()
+
+
 
 def memory_conversational_chat():
-
     session_id = request.headers.get("session-id")
-    conversation_memory = session_manager.get_conversation_memory(session_id)
+    message_history = session_manager.get_conversation_memory(session_id)
     body = request.get_json()
     user_input = body.get("message")
 
-    conversation_memory.append({"role": "user", "content": user_input})
-    
-    # Generate a response from the model using conversational memory
-    response = llm.generate_response(conversation_memory, user_input)
-    
-    conversation_memory.append({"role": "model", "content": response})
+    message_history.add_user_message(str({"role": "user", "content": user_input}))
 
-    session_manager.update_conversation_memory(session_id, conversation_memory)
-    
+    tools = [
+        Tool(
+            name="Current Search",
+            func=search.run,
+            description="useful for when you need to answer questions about current events or the current state of the world"
+        ),
+    ]
+    prefix = """Have a conversation with a human, answering the following questions as best you can. You have access to the following tools:"""
+    suffix = """Begin!"
+
+    {chat_history}
+    Question: {input}
+    {agent_scratchpad}"""
+
+    prompt = ZeroShotAgent.create_prompt(
+        tools,
+        prefix=prefix,
+        suffix=suffix,
+        input_variables=["input", "chat_history", "agent_scratchpad"]
+    )
+
+    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=message_history)
+
+
+    llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
+    agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+    agent_chain = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
+    response = agent_chain.run(user_input)
+
+    message_history.add_ai_message(user_input)
+
     response_data = {
         "response": response,
-        "conversation": conversation_memory
     }
+
+    return jsonify(response_data)   
+
+
+
+    # agent_chain = initialize_agent(create_tools(), llms, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
+    # answer = agent_chain.run(user_input)
+    # print(answer)
+
+    return ""
+
+    pinceConeData = pinecone.getPineConeInstance()
+    pine_cone_result = pinceConeData.similarity_search(user_input)
+    filtered_results = []
+    for result in pine_cone_result:
+        if result.score >= 0.8:
+            filtered_results.append(result.document.page_content)
+    print(filtered_results)
+
+    return ""
+
+    # langchain agent
+    agent = Agent()
+
+    tools = {"pinecone": pine_cone_result, "redis": conversation_memory }
+    response = agent.generate_response(conversation_memory, user_input, tools=tools)
+    print(response)
+    return ""
+
+    # Generate a response from the model using conversational memory
+    response = llm.generate_response(conversation_memory, user_input)
+
     
-    return jsonify(response_data)
--- utils/session_util.py
+++ utils/session_util.py
@@ -1,6 +1,7 @@
 import redis
 import uuid
 import json
+from langchain.memory.chat_message_histories import RedisChatMessageHistory
 
 class SessionManager:
     def __init__(self):
@@ -12,11 +13,12 @@ class SessionManager:
         return session_id
 
     def get_conversation_memory(self, session_id):
-        conversation_memory = self.redis_client.get(session_id)
-        if conversation_memory:
-            return json.loads(conversation_memory)
+        message_history_data = self.redis_client.get(session_id)
+        if message_history_data is not None:
+            message_history_data = message_history_data.decode("utf-8")  # Decode bytes to str
+            message_history = RedisChatMessageHistory(message_history_data)
+            return message_history
         else:
-            return []
+            return RedisChatMessageHistory()
 
-    def update_conversation_memory(self, session_id, conversation_memory):
-        self.redis_client.set(session_id, json.dumps(conversation_memory))
+
