648d6755c3d5e80e1849e98b9d5be9ef5c80e306
Illapavan <pavan@radiusagent.com>
2023-06-23 14:45:53 +0530

    fixed the cors error

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from controllers.memory_chat_controller import memory_conversational_chat
 from controllers.sessions_controller import new_session
 from controllers.image_controller import image_generator 
 
 app = Flask(__name__)
+cors = CORS(app)
 
 app.route('/api/v1/memory-conversational-chat', methods=["POST"])(memory_conversational_chat)
 app.route('/api/v1/new-session', methods=["POST"])(new_session)
