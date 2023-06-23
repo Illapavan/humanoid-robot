69c7a2b9842c5023a68b580b09ff81c0bea51682
Abhinav Aditya <40894851+abhinavaditya811@users.noreply.github.com>
2023-06-23 16:15:03 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from controllers.memory_chat_controller import memory_conversational_chat
+from controllers.memory_chat_controller import memory_conversational_chat, pdf_reader
 from controllers.sessions_controller import new_session
 
 app = Flask(__name__)
 
 app.route('/api/v1/memory-conversational-chat', methods=["POST"])(memory_conversational_chat)
 app.route('/api/v1/new-session', methods=["POST"])(new_session)
+app.route('/api/v1/pdf-reader', methods=["POST"])(pdf_reader)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
--- controllers/memory_chat_controller.py
+++ controllers/memory_chat_controller.py
@@ -1,6 +1,16 @@
 from flask import request, jsonify
+import codecs
+from io import BytesIO
+import boto3
 from utils.openai_util import ChatOpenAI
 from utils.session_util import SessionManager
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 
 llm = ChatOpenAI(temperature=0.8)
 session_manager = SessionManager()
@@ -27,3 +37,44 @@ def memory_conversational_chat():
     }
     
     return jsonify(response_data)
+
+def pdf_reader():
+
+    data = request.get_json()
+    pdf = data.get("pdf")
+    if pdf is not None:
+        s3 = boto3.client("s3")
+        obj = s3.get_object(Bucket="metropoc", Key="Setup+Docker+in+Windows.pdf")
+        print(obj)
+        fs = obj['Body'].read()
+        pdf_reader = PdfReader(BytesIO(fs))
+        # pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+            
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+        
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+        
+        # show user input
+        user_question = data.get("question")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+            
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=user_question)
+                print(cb)
+            print(response)
+    
