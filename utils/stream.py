767d492fe5378aaae114916d2c9fcc69ed54293c
Raghvendra <raghvendra@radiusagent.com>
2023-07-08 17:18:24 +0530

    fix

--- utils/stream.py
+++ utils/stream.py
@@ -52,7 +52,7 @@ def add_bot_to_channel(body):
 
     try:
         channelObject = server_client.query_channels({"cid": body.get("cid")}, limit=1)
-        c = channelObject.get("channels")
+        c = channelObject.get("channels").get(0)
         print(c)
         if channelObject is None or channelObject.get("channels") is None or len(channelObject.get("channels")) != 1 or \
                 len(channelObject.get("channels")[0].get("members")) != 0:
