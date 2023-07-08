5bc93276946f7deede4cc12ffe37e8113d260fa3
Raghvendra <raghvendra@radiusagent.com>
2023-07-08 17:15:51 +0530

    fix

--- utils/stream.py
+++ utils/stream.py
@@ -52,6 +52,8 @@ def add_bot_to_channel(body):
 
     try:
         channelObject = server_client.query_channels({"cid": body.get("cid")}, limit=1)
+        c = channelObject.get("channels")
+        print(c)
         if channelObject is None or channelObject.get("channels") is None or len(channelObject.get("channels")) != 1 or \
                 len(channelObject.get("channels")[0].get("members")) != 0:
             print("Invalid channel for adding bot")
