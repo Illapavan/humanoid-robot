a0e58350856cd7834cbbf9c5f630aeb39292dbdb
Raghvendra <raghvendra@radiusagent.com>
2023-07-11 16:40:14 +0530

    fix

--- utils/stream.py
+++ utils/stream.py
@@ -134,6 +134,7 @@ def message_handler(body):
         if data_type == "chat":
             response = memory_conversational_chat(data)
             print("Second Final comment")
+            print(response)
             send_message(channel_type, channel_id, bot_member_id, response.get("response"))
         elif data_type == "pdf_reader":
             response = pdf_reader(data)
