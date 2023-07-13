a155564aebfa59e0b5bcf42d692f087752e5a205
Illapavan <pavan@radiusagent.com>
2023-07-13 21:46:55 +0530

    removed tables

--- utils/session_util.py
+++ utils/session_util.py
@@ -30,7 +30,7 @@ class SessionManager:
         
         # uri = 'mysql+pymysql://aduser:adxyz123@127.0.0.1:3309/agentdesks'
         uri = self.getDbConnectionURI()
-        db = SQLDatabase.from_uri(uri, include_tables=['scheduled_event_rooms', 'signup_and_login_table', 'constellation_dump'], sample_rows_in_table_info=2)
+        db = SQLDatabase.from_uri(uri, include_tables=['constellation_dump'], sample_rows_in_table_info=2)
         llm = OpenAI(temperature=0, verbose=True)
         db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
         return db_chain
